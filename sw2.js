c91ad0601609901d33fea926cda1000fae4108e5
viviancortesdgdgdg13 <viviancortesdgdgdg13@users.noreply.github.com>
2026-08-21 17:21:10 +0530

    Start Retardo Auto Micro at 0:40

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const cacheName = "1787292249795"; // this gets replaced by the build script
+const cacheName = "1787312874932"; // this gets replaced by the build script
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install", cacheName);
