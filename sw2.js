4f0393fc7457fa3c5254fe9860e17244af0c1a01
viviancortesdgdgdg13 <viviancortesdgdgdg13@users.noreply.github.com>
2026-08-21 19:44:24 +0530

    Separate Retardo from Messiah planning

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const cacheName = "1787314165598"; // this gets replaced by the build script
+const cacheName = "1787321600837"; // this gets replaced by the build script
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install", cacheName);
