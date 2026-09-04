2572f366d3243a4b30f74902e24e7be9b5d6d6a5
viviancortesdgdgdg13 <viviancortesdgdgdg13@users.noreply.github.com>
2026-09-04 10:10:29 +0530

    Extend Absolute 3960 MP through cycle nine

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const cacheName = "1788495632894"; // this gets replaced by the build script
+const cacheName = "1788496647100"; // this gets replaced by the build script
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install", cacheName);
